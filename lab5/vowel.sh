1fa1e90579fb1e89944586ec1b3977b865480071
Anukesh07 <53857067+Anukesh07@users.noreply.github.com>
2020-11-19 13:26:19 +0530

    Create vowel.sh

---
+++ lab5/vowel.sh
@@ -0,0 +1,50 @@
+echo "Enter the letter : "
+while :
+do
+	read x
+	case $x in 
+		"a")
+			echo "Vowel"
+			break 
+			;;
+		"e")
+			echo "Vowel"
+			break
+			;;
+		"i")
+			echo "Vowel"
+			break
+			;;
+		"o")
+			echo "Vowel"
+			break
+			;;
+		"u")
+			echo "Vowel"
+			break
+			;;
+		"A")
+			echo "Vowel"
+			break
+			;;
+		"E")
+			echo "Vowel"
+			break
+			;;
+		"I")
+			echo "Vowel"
+			break
+			;;
+		"O")
+			echo "Vowel"
+			break
+			;;
+		"U")
+			echo "Vowel"
+			break
+			;;
+		*)
+			echo "Consonant"
+			break
+			esac
+		done
